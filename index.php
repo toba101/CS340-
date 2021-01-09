45bc6f393155be74fbc598a863ac957f0369df83
toba101 <toba.obiwale@outlook.com>
2021-01-09 01:43:07 +0000

    First push

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+  <meta charset="utf-8">
+  <title>PHP Motors HomePage | Toba A. Obiwale|CSE 340</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/phpmotors/styles/small.css" type="text/css" rel="stylesheet" media="screen">
+  <link href="/phpmotors/styles/large.css" type="text/css" rel="stylesheet" media="screen">
+<body>
+
+    <header>
+      <div class="homepage">
+            <img src="/phpmotors/images/site/logo.png" alt="PHP Logo">
+      <!-- </div>
+
+      <div class="side-account"> -->
+            <a href="account.php">My Account</a>
+      </div>
+    </header>
+   
+    <main>
+        <nav>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="classic.php">Classic</a></li>
+                <li><a href="sports.php">Sports</a></li>
+                <li><a href="suv.php">SUV</a></li>
+                <li><a href="truck.php">Truck</a></li>
+                <li><a href="used.php">Used</a></li> 
+            </ul>
+        </nav>
+
+        <div class="page-content">
+            <h1> Content Title Here </h1>
+        </div>
+
+    </main>
+
+    <hr>
+    
+    <footer>
+    &copy; PHP Motors, All rights reserved. <br>
+            All images used are believed to be in "Fair Use". Please notify the author if any are not and will be removed.<br>
+   Last  Updated: 30 March . 2018
+    </footer>
+    
+</body>
